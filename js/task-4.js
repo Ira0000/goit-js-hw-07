d19bc418cc1baba69b9acb77e6939ecf3d98e56a
Iryna Liskevych <160422110+Ira0000@users.noreply.github.com>
2024-06-30 10:55:40 +0200

    task 4 add js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const submitForm = document.querySelector('.login-form');
 
+console.dir(submitForm);
+
+submitForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const info = {
+    email: formElements.email.value.trim(),
+    password: formElements.password.value.trim(),
+  };
+  if (info.email === '' || info.password === '') {
+    return alert('All form fields must be filled in');
+  } else {
+    console.log(info);
+  }
+  event.currentTarget.reset();
+}
